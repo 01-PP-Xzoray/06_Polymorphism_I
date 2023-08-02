dbfb707b24fa577c6e1310cf4400788f6f7f032a
Sohrab <sohrab-arenja8@hotmail.de>
2023-08-02 10:59:42 +0200

    iteration

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,14 +8,27 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
 
-        output(queen.DoYourJob());
-        output(worker.DoYourJob());
-        output(drone.DoYourJob());
+        // output(queen.DoYourJob());
+        // output(worker.DoYourJob());
+        // output(drone.DoYourJob());
 
+        pollobj(queen);
+        pollobj(worker);
+        pollobj(drone);
     }
 
+    private static void pollobj(Queen obj) {
+         output(obj.DoYourJob());
+    }
    
-   
+    private static void pollobj(Worker obj) {
+         output(obj.DoYourJob());
+    }
+    
+    private static void pollobj(Drone obj) {
+         output(obj.DoYourJob());
+    }
+
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
